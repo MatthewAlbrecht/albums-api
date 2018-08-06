0b3cca76c3dca2b72cddbea0c956b8a048b5b5e2
Matthew Albrecht <matthewwilliamalbrecht@gmail.com>
2018-08-06 11:18:49 -0600

    troubleshoot spotify data put

--- api/controllers/misc.js
+++ api/controllers/misc.js
@@ -1,67 +1,99 @@
 const { sendResponse } = require("../utils/responseUtils");
 const Albums = require("../models/albums");
 const rp = require("request-promise");
-let querystring = require('querystring')
+let querystring = require("querystring");
 
-let accessToken = "BQC_sPTfAVNBSZ9OrlyTNSScOt4HaxGiL7PIQ2eL_Bw3LZDB6tMMKSm8rxYZROiOXnZ2SHR_QcDBYLqagDv-b8bNNPEr8Jr07EHQnS4oK2TItayzGz_PzLFiemSUqZMyXjW4Rlm-l2cyTGJBed8kXnhRvVas54hT"
+let accessToken =
+  "BQBF7ri0sIXVvQaVlYygrPWT6JJVMLaUarV70OcxtIrkBbkluOtdcTgOioBAKC4IhzL4as16euGcVEK1dhECohmWnhKQzJXJX5s2jLutsB4vkDJxTjCTF83iQnB_w8idN7f4JpAesiPS5SSzLSDelinsIqC77jgw";
 module.exports.spotifyData = async (req, res, next) => {
-  let albums
+  let albums;
   try {
-    albums = await Albums.find({})
+    albums = await Albums.find({});
   } catch (error) {
-    sendResponse(res, 400, error)
-    return
+    sendResponse(res, 400, error);
+    return;
   }
 
-  let qsArrays = [], promises = []
+  let qsArrays = [],
+    promises = [];
   let spotifyURIs = albums
     .filter(album => !album.spotifyAlbumData)
-    .map(album => album.spotifyURI)
+    .map(album => album.spotifyURI);
 
-  while(spotifyURIs.length) {
-    qsArrays.push(spotifyURIs.splice(0,20))
+  while (spotifyURIs.length) {
+    qsArrays.push(spotifyURIs.splice(0, 20));
   }
-  
-  let spotifyAlbums
+
+  let spotifyAlbums;
   promises = qsArrays.map(async qs => {
-    querystring.stringify({ ids: qs })
+    querystring.stringify({ ids: qs });
     let authOptions = {
-      url: 'https://api.spotify.com/v1/albums?' + querystring.stringify({ ids: qs.join(",") }),
+      url:
+        "https://api.spotify.com/v1/albums?" +
+        querystring.stringify({ ids: qs.join(",") }),
       method: "get",
       headers: {
-        "Authorization": "Bearer " + accessToken
+        Authorization: "Bearer " + accessToken
       },
       json: true
-    }
-    return rp(authOptions)
-  })
+    };
+    return rp(authOptions);
+  });
 
   try {
-    spotifyAlbums = await Promise.all(promises)
+    spotifyAlbums = await Promise.all(promises);
   } catch (error) {
-    sendResponse(res, 400, error) 
-    return   
+    sendResponse(res, 400, error);
+    return;
   }
 
-  spotifyAlbums = [].concat(...spotifyAlbums.map(el => el.albums))
-  promises = []
+  spotifyAlbums = [].concat(...spotifyAlbums.map(el => el.albums));
+  promises = [];
   albums = albums
     .map(album => {
-      let uri = album.spotifyURI
-      album.spotifyAlbumData = spotifyAlbums.find(spotifyAlbum => spotifyAlbum.uri.includes(uri))
-      return album
+      let uri = album.spotifyURI;
+      // console.log('\n---> album.albumName <---\n', album.albumName, '\n');
+      album.spotifyAlbumData = spotifyAlbums.find(spotifyAlbum => {
+        if (!spotifyAlbum) {
+          // console.log('\n=== HERE ===\n');
+        }
+        return spotifyAlbum.uri.includes(uri)
+      }
+      );
+      return album;
     })
     .forEach(album => {
-      album = new Albums(album)
-      promises.push(album.save())
-    })
-  
-  let savedAlbums
+      album = new Albums(album);
+      promises.push(album.save());
+    });
+
+  let savedAlbums;
   try {
-    savedAlbums = await Promise.all(promises)
+    savedAlbums = await Promise.all(promises);
   } catch (error) {
-    sendResponse(res, 400, error)
+    sendResponse(res, 400, error);
   }
-  
-  sendResponse(res, 200, savedAlbums[0])
+
+  sendResponse(res, 200, savedAlbums[0]);
 };
+
+module.exports.getGenres = async (req, res, next) => {
+  let genres;
+  try {
+    genres = await Albums
+      .find({})
+      .distinct("genres")
+      .lean();
+  } catch (error) {
+    sendResponse(res, 400, error);
+  }
+  genres = genres.sort(alphabetize)
+  console.log('\n---> genres <---\n', genres, '\n');
+  sendResponse(res, 200, {docs: genres});
+};
+
+function alphabetize(a, b) {
+  if(a < b) return -1;
+  if(a > b) return 1;
+  return 0;
+}
